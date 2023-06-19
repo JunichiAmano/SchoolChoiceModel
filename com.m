ecb22ed24cb37b43343bdc1f68f2330f0e22c32f
JunichiAmano <80298162+JunichiAmano@users.noreply.github.com>
2023-06-18 21:25:21 -0400

    Add files via upload

--- com.m
+++ com.m
@@ -21,8 +21,91 @@ bas = [repelem(colegios,rows(postulantes),1) repmat(postulantes,rows(colegios),1
 y = bas(:,1) == bas(:,14) ;                                                          % crear dummy 'y' de eleccion de colegios
 dist = deg2km(distance(bas(:,5),bas(:,6),bas(:,12),bas(:,13)))' ;                    % calcular distancias entre hogares y colegios
 simce = bas(:,7) + bas(:,8)/2 ;                                                      % calcular promedio simce matemáticas y simce lenguaje
-bas = bas((bas(:,4)==bas(:,3)|(bas(:,10)==bas(:,4)==1)|(bas(:,10)==0&bas(:,5)==1)),:) ;% eliminar opciones inviables (mujeres en colegios de hombres y hombres en colegios de mujeres)
+%bas = bas((bas(:,4)==bas(:,3)|(bas(:,10)==bas(:,4)==1)|(bas(:,10)==0&bas(:,5)==1)),:) ;% eliminar opciones inviables (mujeres en colegios de hombres y hombres en colegios de mujeres)
 
-x = [dist simce bas(:,2) bas(:,11)] ;                                                % regresores: distancia; simce; copago; prioridad
+%x = [dist simce bas(:,2) bas(:,11)] ;                                                % regresores: distancia; simce; copago; prioridad
 
-(x'*x)\(x'*y)                                                                        % regresion preliminar para obtener betas de inicio
+%bi = (x'*x)\(x'*y)                                                                   % regresion preliminar para obtener betas de inicio
+
+x = [simce] ;
+bi = (x'*x)\(x'*y) ;
+tol = 10^(-6) ;
+
+x*bi ;
+
+
+
+n = rows(postulantes) ;
+J = rows(colegios) ;
+
+x = permute(x, [1, 3 , 2]) ;
+x = reshape(x, [n , J , length(x(1,:,:))]) ;
+
+y = reshape(x, [n , J ]) ;
+
+bh = bi ;
+
+
+for h = 1:100
+
+bi = bh ;
+
+
+
+M = zeros(n, 1) ;
+    for i = 1:n
+        sum1 = 0;
+        sum2 = 0;
+        for k = 1:J
+            sum1 = sum1 + x(i, k) * exp(bi * x(i, k));
+            sum2 = sum2 + exp(bi * x(i, k));
+        end
+        M(i) = sum1 / (1 + sum2);
+    end
+
+    G = 0;
+    for i = 1:n
+        for j = 1:J
+            G = G + y(i, j) * (x(i, j) - M(i));
+        end
+    end
+
+
+    M = zeros(n, 1);
+    N = zeros(n, 1);
+
+    for i = 1:n
+        sum1_M = 0;
+        sum2_M = 0;
+        sum1_N = 0;
+        sum2_N = 0;
+        for k = 1:J
+            sum1_M = sum1_M + x(i, k)^2 * exp(bi * x(i, k));
+            sum2_M = sum2_M + exp(bi * x(i, k));
+            sum1_N = sum1_N + x(i, k) * exp(bi * x(i, k));
+            sum2_N = sum2_N + exp(bi * x(i, k));
+        end
+        M(i) = sum1_M / (1 + sum2_M);
+        N(i) = (sum1_N / (1 + sum2_N))^2;
+    end
+
+    H = 0;
+    for i = 1:n
+        for j = 1:J
+            H = H - y(i, j) * (M(i) - N(i)) ;
+        end
+    end
+
+    bh = bi + (-H)^(-1)*G
+
+    if abs(bh - bi) < tol
+      break
+    endif
+
+    if H = 0 ;
+      break
+    endif
+
+end
+
+
